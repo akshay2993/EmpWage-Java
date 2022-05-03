6393b5dfc37f925d7415e74f835068ce814a95c6
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-05-03 09:36:38 +0530

    Successfully refactored using Switch statement.

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,25 +1,27 @@
 package com.bridgelabz.EmployeeWage;
-import java.lang.Math;
 
 public class EmployeeWage {
 	
 	public static void main(String[] args) {
 		
-		int IS_ABSENT = 0;
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int WAGE_PER_HOUR = 20;
+		final int IS_FULL_TIME = 1;
+		final int IS_PART_TIME = 2;
+		final int WAGE_PER_HOUR = 20;
 		int empHrs = 0;
 		int dailyWage = 0;
-		double empCheck = Math.floor(Math.random()*3);
+		int empCheck = (int)Math.floor(Math.random()*3);
 		
-		if(empCheck == IS_ABSENT)
-			empHrs = 0;
-		else if (empCheck == IS_FULL_TIME)
-			empHrs = 8;
-		else 
-			empHrs = 4;
-		
+		switch (empCheck) {
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			default:
+				empHrs = 0;
+		}
+
 		dailyWage += empHrs*WAGE_PER_HOUR ;
 		System.out.println("Employee's salary today: "+ dailyWage +" $");
 		}
