26a08500ad9a3bd72b649e983ac00cc7f9a843b7
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-05-03 10:01:04 +0530

    Added Monthly Wage Calculation.

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,22 +7,29 @@ public class EmployeeWage {
 		final int IS_FULL_TIME = 1;
 		final int IS_PART_TIME = 2;
 		final int WAGE_PER_HOUR = 20;
+		final int NUM_OF_WORKING_DAYS = 20;
 		int empHrs = 0;
 		int dailyWage = 0;
-		int empCheck = (int)Math.floor(Math.random()*3);
+		int totalWage = 0;
 		
-		switch (empCheck) {
-			case IS_FULL_TIME:
-				empHrs = 8;
-				break;
-			case IS_PART_TIME:
-				empHrs = 4;
-				break;
-			default:
-				empHrs = 0;
-		}
+		for(int day = 1; day <= NUM_OF_WORKING_DAYS; day++) {
+			int empCheck = (int)Math.floor(Math.random()*3);
+			
+			switch (empCheck) {
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				default:
+					empHrs = 0;
+			}
 
-		dailyWage += empHrs*WAGE_PER_HOUR ;
-		System.out.println("Employee's salary today: "+ dailyWage +" $");
+			dailyWage = empHrs*WAGE_PER_HOUR ;
+			totalWage += dailyWage ; 
+			System.out.println("Employee's salary today: "+ dailyWage +" $");
+		}
+		System.out.println("Employee's total monthly salary: "+ totalWage +" $");
 		}
 	}
