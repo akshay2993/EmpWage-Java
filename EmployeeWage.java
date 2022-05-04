b193380ec30e9d316ab1f05e2a0d5daa7b5e5824
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-05-04 07:23:29 +0530

    Added condition of max monthly Hrs/Days

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -8,11 +8,16 @@ public class EmployeeWage {
 		final int IS_PART_TIME = 2;
 		final int WAGE_PER_HOUR = 20;
 		final int NUM_OF_WORKING_DAYS = 20;
+		final int MAX_MONTHLY_HRS = 100;
 		int empHrs = 0;
 		int dailyWage = 0;
 		int totalWage = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
 		
-		for(int day = 1; day <= NUM_OF_WORKING_DAYS; day++) {
+		//for(int day = 1; day <= NUM_OF_WORKING_DAYS; day++) 
+		while (totalEmpHrs <= MAX_MONTHLY_HRS && totalWorkingDays <= NUM_OF_WORKING_DAYS ) {
+			totalWorkingDays++;
 			int empCheck = (int)Math.floor(Math.random()*3);
 			
 			switch (empCheck) {
@@ -25,6 +30,8 @@ public class EmployeeWage {
 				default:
 					empHrs = 0;
 			}
+			totalEmpHrs += empHrs;
+			System.out.println("Day No.:"+ totalWorkingDays + " Employee Hrs: " + empHrs);
 
 			dailyWage = empHrs*WAGE_PER_HOUR ;
 			totalWage += dailyWage ; 
